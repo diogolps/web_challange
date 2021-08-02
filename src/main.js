35a776262633ac39d535d53b2d01be2ce664ce6a
diogolps <79461570+diogolps@users.noreply.github.com>
2021-08-02 16:23:38 +0100

    commit on master

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./routes/index";
+import firebase from "firebase";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBDYESWfS7b2A9TeyD9SSLqaPfkzLEqz1g",
+  authDomain: "web-challenge-9d8c4.firebaseapp.com",
+  projectId: "web-challenge-9d8c4",
+  storageBucket: "web-challenge-9d8c4.appspot.com",
+  messagingSenderId: "447845990637",
+  appId: "1:447845990637:web:30568c5b46f96c5c588ce4",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged((user) => {
+  store.dispatch("fetchUser", user);
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
