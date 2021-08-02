032a0960630203868308b13f28e62e733627e274
diogolps <79461570+diogolps@users.noreply.github.com>
2021-08-02 16:45:54 +0100

    commit master

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import App from "./App.vue";
 import router from "./routes/index";
 import firebase from "firebase";
 import store from "./store";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
